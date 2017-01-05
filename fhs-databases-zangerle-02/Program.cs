d9c60c0ff5d8d48ab750f3ec4b4b207e7383c9a2
Stephan Sum <sum@live.de>
2017-01-05 11:43:33 +0100

    init

---
+++ fhs-databases-zangerle-02/Program.cs
@@ -0,0 +1,97 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fhs_databases_zangerle_02
+{
+    class Program
+    {
+
+        public static readonly string ExePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        public static readonly string ExeDir = Path.GetDirectoryName(ExePath);
+
+        public static readonly string DataMiniDirName = "meetup-data-mini";
+        public static readonly string DataExtractDirName = "meetup-data-extract";
+        public static readonly string DataFullDirName = "meetup-data-full";
+
+        static void Main(string[] args)
+        {
+            //SeedDatabase();
+            Test(Path.Combine(DataExtractDirName, "members"));
+
+            Console.ReadLine();
+        }
+
+
+        public static void SeedDatabase()
+        {
+            SeedMembers();
+        }
+
+        public static void Test(string path)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Start parsing file: {file}");
+                var jsonRaw = ReadFile(file);
+                jsonRaw = jsonRaw.Replace("'", "''");
+                Console.WriteLine(jsonRaw);
+            }
+            
+        }
+
+
+        public static string ReadFile(string filename)
+        {
+            using (StreamReader r = new StreamReader(filename))
+            {
+                return r.ReadToEnd();
+            }
+        }
+
+
+        public static void SeedMembers()
+        {
+            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "pass")))
+            using (var session = driver.Session())
+            {
+                session.Run("CREATE (a:Person {name:'Arthur', title:'King'})");
+                var result = session.Run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title");
+
+                foreach (var record in result)
+                {
+                    Console.WriteLine($"{record["title"].As<string>()} {record["name"].As<string>()}");
+                }
+            }
+        }
+
+
+        public static void RsvpFolderFlatter()
+        {
+            var dataSetPath = Path.Combine(ExeDir, DataExtractDirName);
+            var rsvpFolderPath = Path.Combine(dataSetPath, "rsvps");
+
+            var folders = Directory.GetDirectories(rsvpFolderPath);
+
+            var index = 1;
+            foreach (var folder in folders)
+            {
+                Console.WriteLine($"{index}/{folders.Length}: Flat folder: {folder}");
+                var files = Directory.GetFiles(folder);
+                foreach (var file in files)
+                {
+                    var destinationPath = Path.Combine(rsvpFolderPath, Path.GetFileName(file));
+                    File.Move(file, destinationPath);
+                }
+                Directory.Delete(folder);
+                index++;
+            }
+            Console.WriteLine("Flattening finished.");
+        }
+    }
+}
---
+++ fhs-databases-zangerle-02/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("fhs-databases-zangerle-02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fhs-databases-zangerle-02")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("edaa9f0d-c268-4d46-8403-e29ffb171179")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
